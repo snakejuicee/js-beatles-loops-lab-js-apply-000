b963956d95878d99c771fdd3d348af2f18a40c1d
Patricia Arnedo <paaarnedo@gmail.com>
2020-09-05 22:22:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ function theBeatlesPlay(a,b) {
   var s = [];
   
   for (var i = 0; i < a.length; i++) {
-    s.push(a[i] + " plays " + b[i]);
+    // s.push(a[i] + " plays " + b[i]);
+    s.push(`${a[i]} plays ${b[i]}`)
   }
   return s;
 }
